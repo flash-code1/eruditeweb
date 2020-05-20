4a0d16c869b70a582a1d3df98415d907d22962f5
flash code <47109193+flash-code1@users.noreply.github.com>
2020-05-20 16:17:40 +0100

    gogo

---
+++ index.php
@@ -0,0 +1,301 @@
+<?php
+include("header.php");
+?>
+ <main class="side-main">
+    <!--================ Hero sm Banner start =================-->      
+    <section class="hero-banner mb-30px">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            <div class="hero-banner__img">
+              <img class="img-fluid" src="img/banner/hero-banner.png" alt="">
+            </div>
+          </div>
+          <div class="col-lg-5 pt-5">
+            <div class="hero-banner__content">
+              <h1>Best E-learning Application</h1>
+              <p>Erudite Tutors | Professonal Courses - Digital Marketing | ICAN <br> 
+              Secoundary Schools - SS1 | SS2 | SS3 <br> 
+            Examination - WAEC | JAMB | NECO</p>
+              <a class="button bg" href="#">Get Started</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ Hero sm Banner end =================-->
+
+    
+    <!--================ Feature section start =================-->      
+    <section class="section-margin">
+      <div class="container">
+        <div class="section-intro pb-85px text-center">
+          <h2 class="section-intro__title">Creativity and Intelligence</h2>
+          <p class="section-intro__subtitle">We Help our Students To Fullfill Dreams, And we also Help them Grow Academically.
+              Erudite Tutors Has Impacted so Much in Students All over the Globe.
+          </p>
+        </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <div class="card card-feature text-center text-lg-left mb-4 mb-lg-0">
+                <span class="card-feature__icon">
+                  <i class="ti-package"></i>
+                </span>
+                <h3 class="card-feature__title">Courses</h3>
+                <p class="card-feature__subtitle">Erudite Has About 100,000 Accredited Courses</p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="card card-feature text-center text-lg-left mb-4 mb-lg-0">
+                <span class="card-feature__icon">
+                  <i class="ti-mouse-alt"></i>
+                </span>
+                <h3 class="card-feature__title">Study Guide</h3>
+                <p class="card-feature__subtitle">Erudite Helps Students To grow their Creativity, Nothing is Hard with Erudite</p>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="card card-feature text-center text-lg-left mb-4 mb-lg-0">
+                <span class="card-feature__icon">
+                  <i class="ti-headphone-alt"></i>
+                </span>
+                <h3 class="card-feature__title">Customer Support</h3>
+                <p class="card-feature__subtitle">Erudite Tutors are Online 24/7 fro YOU</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ Feature section end =================-->      
+     <!--================ about section start =================-->      
+     <section class="section-padding--small bg-magnolia">
+      <div class="container">
+        <div class="row no-gutters align-items-center">
+          <div class="col-md-5 mb-5 mb-md-0">
+            <div class="about__content">
+              <h2>Leading the Digital E-Learning Revolution</h2>
+              <p>Dummy Document, Here i Need Little Story About Erudite. Here i Need Little Story About Erudite, Here i Need Little Story About Erudite</p>
+              <a class="button button-light" href="#">Know More</a>
+            </div>
+          </div>
+          <div class="col-md-7">
+            <div class="about__img">
+              <img class="img-fluid" src="design/img/home/about.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ about section end =================-->      
+
+     <!--================ Offer section start =================-->      
+     <section class="section-margin">
+      <div class="container">
+        <div class="section-intro pb-85px text-center">
+          <h2 class="section-intro__title">Features We Offer</h2>
+          <p class="section-intro__subtitle">We make Learning from your Home Comfortable and to Your Taste, by Providing All this Kits</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6">
+
+            <div class="row offer-single-wrapper">
+              <div class="col-lg-6 offer-single">
+                <div class="card offer-single__content text-center">
+                  <span class="offer-single__icon">
+                    <i class="ti-pencil-alt"></i>
+                  </span>
+                  <h4>Study Kit</h4>
+                  <p>Once you Register as a Student, You Get Free E-learning Materials.</p>
+                </div>
+              </div>
+              
+              <div class="col-lg-6 offer-single">
+                <div class="card offer-single__content text-center">
+                  <span class="offer-single__icon">
+                    <i class="ti-ruler-pencil"></i>
+                  </span>
+                  <h4>Analytics Tool</h4>
+                  <p>We make sure you are Always On Par with studies, You have the Ability to Track your Learning Progress, by Taking Test | Assignments Etc.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="row offer-single-wrapper">
+              <div class="col-lg-6 offer-single">
+                <div class="card offer-single__content text-center">
+                  <span class="offer-single__icon">
+                    <i class="ti-cut"></i>
+                  </span>
+                  <h4>Professionals Tools</h4>
+                  <p>Good Sound Quality, Online Live Class. Fully Engaged with Student</p>
+                </div>
+              </div>
+              
+              <div class="col-lg-6 offer-single">
+                <div class="card offer-single__content text-center">
+                  <span class="offer-single__icon">
+                    <i class="ti-light-bulb"></i>
+                  </span>
+                  <h4>Ready Content</h4>
+                  <p>We Make sure our Teaching are Anambigious to all Learners.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-lg-6">
+            <div class="offer-single__img">
+              <img class="img-fluid" src="design/img/home/offer.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ Offer section end =================-->      
+
+      <!--================ Solution section start =================-->      
+      <section class="section-padding--small bg-magnolia">
+      <div class="container">
+        <div class="row align-items-center pt-xl-3 pb-xl-5">
+          <div class="col-lg-6">
+            <div class="solution__img text-center text-lg-left mb-4 mb-lg-0">
+              <img class="img-fluid" src="design/img/home/solution.png" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="solution__content">
+              <h2>Knowlegde Growth with Erudite</h2>
+              <p>We Engage you with top solid real time Questions, be it Weac, Jamb, Neco, SAT, NABTEB, ICAN ....</p>
+              <a class="button button-light" href="#">Know More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ Solution section end =================-->    
+
+    <!--================ Pricing section start =================-->      
+    <section class="section-margin">
+      <div class="container">
+        <div class="section-intro pb-85px text-center">
+          <h2 class="section-intro__title">Choose Your Membership</h2>
+          <p class="section-intro__subtitle">Note. you can always Upgrade your Membership to get access to more Learning Features</p>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="card text-center card-pricing">
+              <div class="card-pricing__header">
+                <h4>Freshers</h4>
+                <p>Per Month</p>
+                <h1 class="card-pricing__price"><span>&#8358;</span>1,000.00</h1>
+              </div>
+              <ul class="card-pricing__list">
+                <li> <i class="ti-check"></i>Starter Kit</li>
+                <li> <i class="ti-check"></i> Session 3 Times Weekly</li>
+                <li class="unvalid"><i class="ti-close"></i>Unlimited Hours & Private Session</li>
+                <li class="unvalid"><i class="ti-close"></i>Live Video Class</li>
+                <li class="unvalid"><i class="ti-close"></i>Paid Certificate</li>
+              </ul>
+              <div class="card-pricing__footer">
+                <button class="button button-light">Get Started</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="card text-center card-pricing">
+              <div class="card-pricing__header">
+                <h4>Smiles</h4>
+                <p>Per Month</p>
+                <h1 class="card-pricing__price"><span>&#8358;</span>8,000.00</h1>
+              </div>
+              <ul class="card-pricing__list">
+                <li> <i class="ti-check"></i>Starter Kit</li>
+                <li> <i class="ti-check"></i> Session 3 Times Weekly</li>
+                <li class="unvalid"><i class="ti-close"></i>Unlimited Hours & Private Session</li>
+                <li><i class="ti-check"></i>Live Video Class</li>
+                <li class="unvalid"><i class="ti-close"></i>Paid Certificate</li>
+              </ul>
+              <div class="card-pricing__footer">
+                <button class="button button-light">Get Started</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+            <div class="card text-center card-pricing">
+              <div class="card-pricing__header">
+                <h4>Ultimate</h4>
+                <p>Per Month</p>
+                <h1 class="card-pricing__price"><span>&#8358;</span>15,000.00</h1>
+              </div>
+              <ul class="card-pricing__list">
+                <li> <i class="ti-check"></i>Starter Kit</li>
+                <li> <i class="ti-check"></i> Session 3 Times Weekly</li>
+                <li><i class="ti-check"></i>Unlimited Hours & Private Session</li>
+                <li><i class="ti-check"></i>Live Video Class</li>
+                <li><i class="ti-check"></i>Paid Certificate</li>
+              </ul>
+              <div class="card-pricing__footer">
+                <button class="button button-light">Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ Pricing section end =================-->      
+
+     <!--================ Testimonial section start =================-->      
+     <section class="section-padding bg-magnolia">
+      <div class="container">
+        <div class="section-intro pb-5 text-center">
+          <h2 class="section-intro__title">Our Students Comment</h2>
+          <p class="section-intro__subtitle">Here we will Put the Top Student Comment. </p>
+        </div>
+
+        <div class="owl-carousel owl-theme testimonial">
+          <div class="testimonial__item text-center">
+            <div class="testimonial__img">
+              <img src="design/img/testimonial/testimonial1.jpg" alt="">
+            </div>
+            <div class="testimonial__content">
+              <h3>Mike Abeche</h3>
+              <p>Executive, ACI Group</p>
+              <p class="testimonial__i">Also made from. Give may saying meat there from heaven it lights face had is gathered god earth light for life may itself shall whales made they're blessed whales also made from give may saying meat. There from heaven it lights face had also made from. Give may saying meat there from heaven</p>
+            </div>
+          </div>
+          <div class="testimonial__item text-center">
+            <div class="testimonial__img">
+              <img src="design/img/testimonial/testimonial1.jpg" alt="">
+            </div>
+            <div class="testimonial__content">
+              <h3>Zuby Weac</h3>
+              <p>Karu Secondary School, Abuja</p>
+              <p class="testimonial__i">Also made from. Give may saying meat there from heaven it lights face had is gathered god earth light for life may itself shall whales made they're blessed whales also made from give may saying meat. There from heaven it lights face had also made from. Give may saying meat there from heaven</p>
+            </div>
+          </div>
+          <div class="testimonial__item text-center">
+            <div class="testimonial__img">
+              <img src="design/img/testimonial/testimonial1.jpg" alt="">
+            </div>
+            <div class="testimonial__content">
+              <h3>Stephen Mcmilan</h3>
+              <p>Executive, ACI Group</p>
+              <p class="testimonial__i">Also made from. Give may saying meat there from heaven it lights face had is gathered god earth light for life may itself shall whales made they're blessed whales also made from give may saying meat. There from heaven it lights face had also made from. Give may saying meat there from heaven</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--================ Testimonial section end =================-->      
+
+
+<?php
+include("footer.php");
+?>
