b6aa511e74e9733478f4ec6f96176e5956fae2ca
Ajiboye samuel <sunnyboye2015@gmail.com>
2020-06-25 19:28:08 +0100

    give me news

---
+++ blog_logic.php
@@ -0,0 +1,152 @@
+<div class="col-lg-4">
+                  <div class="blog_right_sidebar">
+                      <aside class="single_sidebar_widget search_widget">
+                          <div class="input-group">
+                              <input type="text" class="form-control" placeholder="Search Posts">
+                              <span class="input-group-btn">
+                                  <button class="btn btn-default" type="button">
+                                      <i class="lnr lnr-magnifier"></i>
+                                  </button>
+                              </span>
+                          </div>
+                          <!-- /input-group -->
+                          <div class="br"></div>
+                      </aside>
+                      <aside class="single_sidebar_widget author_widget">
+                          <img class="author_img rounded-circle" src="design/img/blog/author.png" alt="">
+                          <h4>Oluwaseun</h4>
+                          <p>Senior blog writer</p>
+                          <div class="social_icon">
+                              <a href="#">
+                                  <i class="fab fa-facebook-f"></i>
+                              </a>
+                              <a href="#">
+                                  <i class="fab fa-twitter"></i>
+                              </a>
+                              <a href="https://github.com/flash-code1">
+                                  <i class="fab fa-github"></i>
+                              </a>
+                              <a href="#">
+                                <i class="fab fa-behance"></i>
+                              </a>
+                          </div>
+                          <p>A Technical Guy
+                          </p>
+                          <div class="br"></div>
+                      </aside>
+                      <aside class="single_sidebar_widget popular_post_widget">
+                          <h3 class="widget_title">Popular Posts</h3>
+                          <div class="media post_item">
+                              <img src="design/img/blog/popular-post/post1.jpg" alt="post">
+                              <div class="media-body">
+                                  <a href="single-blog.php">
+                                      <h3>Space The Final Frontier</h3>
+                                  </a>
+                                  <p>02 Hours ago</p>
+                              </div>
+                          </div>
+                          <div class="media post_item">
+                              <img src="design/img/blog/popular-post/post2.jpg" alt="post">
+                              <div class="media-body">
+                                  <a href="single-blog.php">
+                                      <h3>The Amazing Hubble</h3>
+                                  </a>
+                                  <p>02 Hours ago</p>
+                              </div>
+                          </div>
+                          <div class="media post_item">
+                              <img src="design/img/blog/popular-post/post3.jpg" alt="post">
+                              <div class="media-body">
+                                  <a href="single-blog.php">
+                                      <h3>Astronomy Or Astrology</h3>
+                                  </a>
+                                  <p>03 Hours ago</p>
+                              </div>
+                          </div>
+                          <div class="media post_item">
+                              <img src="design/img/blog/popular-post/post4.jpg" alt="post">
+                              <div class="media-body">
+                                  <a href="single-blog.php">
+                                      <h3>Asteroids telescope</h3>
+                                  </a>
+                                  <p>01 Hours ago</p>
+                              </div>
+                          </div>
+                          <div class="br"></div>
+                      </aside>
+                      <aside class="single_sidebar_widget ads_widget">
+                          <a href="#">
+                              <img class="img-fluid" src="design/img/blog/add.jpg" alt="">
+                          </a>
+                          <div class="br"></div>
+                      </aside>
+                      <aside class="single_sidebar_widget post_category_widget">
+                          <h4 class="widget_title">Post Catgories</h4>
+                          <ul class="list cat-list">
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Technology</p>
+                                      <p>37</p>
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Lifestyle</p>
+                                      <p>24</p>
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Fashion</p>
+                                      <p>59</p>
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Art</p>
+                                      <p>29</p>
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Food</p>
+                                      <p>15</p>
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Architecture</p>
+                                      <p>09</p>
+                                  </a>
+                              </li>
+                              <li>
+                                  <a href="#" class="d-flex justify-content-between">
+                                      <p>Adventure</p>
+                                      <p>44</p>
+                                  </a>
+                              </li>
+                          </ul>
+                          <div class="br"></div>
+                      </aside>
+                      <aside class="single-sidebar-widget newsletter_widget">
+                          <h4 class="widget_title">Newsletter</h4>
+                          <p>
+                              Here, I focus on a range of items and features that we use in life without giving them a second thought.
+                          </p>
+                          <div class="form-group d-flex flex-row">
+                              <div class="input-group">
+                                  <div class="input-group-prepend">
+                                      <div class="input-group-text">
+                                          <i class="fa fa-envelope" aria-hidden="true"></i>
+                                      </div>
+                                  </div>
+                                  <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email address" onfocus="this.placeholder = ''"
+                                      onblur="this.placeholder = 'Enter email'">
+                              </div>
+                              <a href="#" class="bbtns">Subcribe</a>
+                          </div>
+                          <p class="text-bottom">You can unsubscribe at any time</p>
+                          <div class="br"></div>
+                      </aside>
+                  </div>
+              </div>
--- news.php
+++ news.php
@@ -63,13 +63,13 @@ include("header.php");
                               <div class="blog_post">
                                   <img src="design/img/blog/main-blog/m-blog-1.jpg" alt="">
                                   <div class="blog_details">
-                                      <a href="single-blog.html">
+                                      <a href="single-blog.php">
                                           <h2>Astronomy Binoculars A Great Alternative</h2>
                                       </a>
                                       <p>MCSE boot camps have its supporters and its detractors. Some people do not understand
                                           why you should have to spend money on boot camp when you can get the MCSE study
                                           materials yourself at a fraction.</p>
-                                      <a class="button button-blog" href="single-blog.html">View More</a>
+                                      <a class="button button-blog" href="single-blog.php">View More</a>
                                   </div>
                               </div>
                           </div>
@@ -111,13 +111,13 @@ include("header.php");
                               <div class="blog_post">
                                   <img src="design/img/blog/main-blog/m-blog-2.jpg" alt="">
                                   <div class="blog_details">
-                                      <a href="single-blog.html">
+                                      <a href="single-blog.php">
                                           <h2>The Basics Of Buying A Telescope</h2>
                                       </a>
                                       <p>MCSE boot camps have its supporters and its detractors. Some people do not understand
                                           why you should have to spend money on boot camp when you can get the MCSE study
                                           materials yourself at a fraction.</p>
-                                      <a href="single-blog.html" class="button button-blog">View More</a>
+                                      <a href="single-blog.php" class="button button-blog">View More</a>
                                   </div>
                               </div>
                           </div>
@@ -159,13 +159,13 @@ include("header.php");
                               <div class="blog_post">
                                   <img src="design/img/blog/main-blog/m-blog-3.jpg" alt="">
                                   <div class="blog_details">
-                                      <a href="single-blog.html">
+                                      <a href="single-blog.php">
                                           <h2>The Glossary Of Telescopes</h2>
                                       </a>
                                       <p>MCSE boot camps have its supporters and its detractors. Some people do not understand
                                           why you should have to spend money on boot camp when you can get the MCSE study
                                           materials yourself at a fraction.</p>
-                                      <a href="single-blog.html" class="button button-blog">View More</a>
+                                      <a href="single-blog.php" class="button button-blog">View More</a>
                                   </div>
                               </div>
                           </div>
@@ -207,13 +207,13 @@ include("header.php");
                               <div class="blog_post">
                                   <img src="design/img/blog/main-blog/m-blog-4.jpg" alt="">
                                   <div class="blog_details">
-                                      <a href="single-blog.html">
+                                      <a href="single-blog.php">
                                           <h2>The Night Sky</h2>
                                       </a>
                                       <p>MCSE boot camps have its supporters and its detractors. Some people do not understand
                                           why you should have to spend money on boot camp when you can get the MCSE study
                                           materials yourself at a fraction.</p>
-                                      <a href="single-blog.html" class="button button-blog">View More</a>
+                                      <a href="single-blog.php" class="button button-blog">View More</a>
                                   </div>
                               </div>
                           </div>
@@ -255,13 +255,13 @@ include("header.php");
                               <div class="blog_post">
                                   <img src="design/img/blog/main-blog/m-blog-5.jpg" alt="">
                                   <div class="blog_details">
-                                      <a href="single-blog.html">
+                                      <a href="single-blog.php">
                                           <h2>Telescopes 101</h2>
                                       </a>
                                       <p>MCSE boot camps have its supporters and its detractors. Some people do not understand
                                           why you should have to spend money on boot camp when you can get the MCSE study
                                           materials yourself at a fraction.</p>
-                                      <a href="single-blog.html" class="button button-blog">View More</a>
+                                      <a href="single-blog.php" class="button button-blog">View More</a>
                                   </div>
                               </div>
                           </div>
@@ -301,158 +301,11 @@ include("header.php");
                       </nav>
                   </div>
               </div>
-              <div class="col-lg-4">
-                  <div class="blog_right_sidebar">
-                      <aside class="single_sidebar_widget search_widget">
-                          <div class="input-group">
-                              <input type="text" class="form-control" placeholder="Search Posts">
-                              <span class="input-group-btn">
-                                  <button class="btn btn-default" type="button">
-                                      <i class="lnr lnr-magnifier"></i>
-                                  </button>
-                              </span>
-                          </div>
-                          <!-- /input-group -->
-                          <div class="br"></div>
-                      </aside>
-                      <aside class="single_sidebar_widget author_widget">
-                          <img class="author_img rounded-circle" src="design/img/blog/author.png" alt="">
-                          <h4>Oluwaseun</h4>
-                          <p>Senior blog writer</p>
-                          <div class="social_icon">
-                              <a href="#">
-                                  <i class="fab fa-facebook-f"></i>
-                              </a>
-                              <a href="#">
-                                  <i class="fab fa-twitter"></i>
-                              </a>
-                              <a href="https://github.com/flash-code1">
-                                  <i class="fab fa-github"></i>
-                              </a>
-                              <a href="#">
-                                <i class="fab fa-behance"></i>
-                              </a>
-                          </div>
-                          <p>A Technical Guy
-                          </p>
-                          <div class="br"></div>
-                      </aside>
-                      <aside class="single_sidebar_widget popular_post_widget">
-                          <h3 class="widget_title">Popular Posts</h3>
-                          <div class="media post_item">
-                              <img src="design/img/blog/popular-post/post1.jpg" alt="post">
-                              <div class="media-body">
-                                  <a href="single-blog.html">
-                                      <h3>Space The Final Frontier</h3>
-                                  </a>
-                                  <p>02 Hours ago</p>
-                              </div>
-                          </div>
-                          <div class="media post_item">
-                              <img src="design/img/blog/popular-post/post2.jpg" alt="post">
-                              <div class="media-body">
-                                  <a href="single-blog.html">
-                                      <h3>The Amazing Hubble</h3>
-                                  </a>
-                                  <p>02 Hours ago</p>
-                              </div>
-                          </div>
-                          <div class="media post_item">
-                              <img src="design/img/blog/popular-post/post3.jpg" alt="post">
-                              <div class="media-body">
-                                  <a href="single-blog.html">
-                                      <h3>Astronomy Or Astrology</h3>
-                                  </a>
-                                  <p>03 Hours ago</p>
-                              </div>
-                          </div>
-                          <div class="media post_item">
-                              <img src="design/img/blog/popular-post/post4.jpg" alt="post">
-                              <div class="media-body">
-                                  <a href="single-blog.html">
-                                      <h3>Asteroids telescope</h3>
-                                  </a>
-                                  <p>01 Hours ago</p>
-                              </div>
-                          </div>
-                          <div class="br"></div>
-                      </aside>
-                      <aside class="single_sidebar_widget ads_widget">
-                          <a href="#">
-                              <img class="img-fluid" src="design/img/blog/add.jpg" alt="">
-                          </a>
-                          <div class="br"></div>
-                      </aside>
-                      <aside class="single_sidebar_widget post_category_widget">
-                          <h4 class="widget_title">Post Catgories</h4>
-                          <ul class="list cat-list">
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Technology</p>
-                                      <p>37</p>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Lifestyle</p>
-                                      <p>24</p>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Fashion</p>
-                                      <p>59</p>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Art</p>
-                                      <p>29</p>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Food</p>
-                                      <p>15</p>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Architecture</p>
-                                      <p>09</p>
-                                  </a>
-                              </li>
-                              <li>
-                                  <a href="#" class="d-flex justify-content-between">
-                                      <p>Adventure</p>
-                                      <p>44</p>
-                                  </a>
-                              </li>
-                          </ul>
-                          <div class="br"></div>
-                      </aside>
-                      <aside class="single-sidebar-widget newsletter_widget">
-                          <h4 class="widget_title">Newsletter</h4>
-                          <p>
-                              Here, I focus on a range of items and features that we use in life without giving them a second thought.
-                          </p>
-                          <div class="form-group d-flex flex-row">
-                              <div class="input-group">
-                                  <div class="input-group-prepend">
-                                      <div class="input-group-text">
-                                          <i class="fa fa-envelope" aria-hidden="true"></i>
-                                      </div>
-                                  </div>
-                                  <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Enter email address" onfocus="this.placeholder = ''"
-                                      onblur="this.placeholder = 'Enter email'">
-                              </div>
-                              <a href="#" class="bbtns">Subcribe</a>
-                          </div>
-                          <p class="text-bottom">You can unsubscribe at any time</p>
-                          <div class="br"></div>
-                      </aside>
-                  </div>
-              </div>
+              <!-- ENNNN -->
+             <?php
+             include("blog_logic.php");
+             ?>
+              <!-- ENNNNN -->
           </div>
       </div>
   </section>
